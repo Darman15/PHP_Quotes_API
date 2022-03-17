05a909612e4b279f9ad05e2314c90f2603858b49
Darman15 <59400245+Darman15@users.noreply.github.com>
2022-03-17 12:24:29 -0500

    Update Database.php

--- config/Database.php
+++ config/Database.php
@@ -1,9 +1,9 @@
 <?php
 
 class Database {
-private $host = 'localhost';
-private $db_name = 'quotesdb';
-private $username = 'root';
+private $host = 'acw2033ndw0at1t7.cbetxkdyhwsb.us-east-1.rds.amazonaws.com';
+private $db_name = 'r8y2bi4z7seeo5oq';
+private $username = 'r7yi52dsre3so2hn';
 private $password = 'Rutten26!';
 private $conn;
 
