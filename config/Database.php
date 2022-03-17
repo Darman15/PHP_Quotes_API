0b1f51d18cf209a88bb65f67e4b7413c85fb5170
Benjamin Parks <Benparks101@gamil.comgit config --global user.email Benparks101@gamil.com>
2022-03-17 13:36:44 -0500

    fix password attempt

--- config/Database.php
+++ config/Database.php
@@ -4,7 +4,7 @@ class Database {
 private $host = 'acw2033ndw0at1t7.cbetxkdyhwsb.us-east-1.rds.amazonaws.com';
 private $db_name = 'r8y2bi4z7seeo5oq';
 private $username = 'r7yi52dsre3so2hn';
-private $password = getenv('db_password');
+private $password = getenv(db_password);
 private $conn;
 
 
