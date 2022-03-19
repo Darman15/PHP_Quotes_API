50652812b529b2379aeaa16bc041d2fcfa757fab
Benjamin Parks <Benparks101@gamil.comgit config --global user.email Benparks101@gamil.com>
2022-03-19 11:13:38 -0500

    update quotes.php

--- api/categories/categories.php
+++ api/categories/categories.php
@@ -34,7 +34,7 @@ if($num > 0) {
     
     }
 
-    print_r(echo json_encode($category_arr));
+    echo json_encode($category_arr);
     echo json_encode(
         array('message' => 'All categories with their ids')
     );
