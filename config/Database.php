0073dbed83719a31e457a6b6f103fd65b56c6ef1
Benjamin Parks <Benparks101@gamil.comgit config --global user.email Benparks101@gamil.com>
2022-03-18 20:23:54 -0500

    update DB Conn

--- config/Database.php
+++ config/Database.php
@@ -20,9 +20,9 @@ class Database {
         // Create your new PDO connection here
         // This is also from the Heroku docs showing the PDO connection: 
         try {
-          $conn = new PDO("mysql:host=$hostname;dbname=$database", $username, $password);
+          $this->conn = new PDO("mysql:host=$hostname;dbname=$database", $username, $password);
           // set the PDO error mode to exception
-          $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+          $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
           echo "Connected successfully";
         }
         catch(PDOException $e)
