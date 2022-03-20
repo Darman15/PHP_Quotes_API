acf0231c7d0c89772b3e2cf2439c6e66b33525c6
Benjamin Parks <Benparks101@gamil.comgit config --global user.email Benparks101@gamil.com>
2022-03-20 00:41:23 -0500

    Fix headers

--- api/categories/index.php
+++ api/categories/index.php
@@ -1,5 +1,11 @@
 <?php
 
+header('Access-Control-Allow-Origin: *');
+header('Content-Type: application/json');
+header('Access-Control-Allow-Methods: POST');
+header('Access-Control-Allow-Methods: PUT');
+header('Access-Control-Allow-Methods: DELETE');
+header('Access-Control-Allow-Headers: Access-Control-Allow-Origin,Content-Type, Access-Control-Allow-Methods,Authorization, X-Requested-With');
 // Set teh request method to variable to use for redirect
 $method = $_SERVER['REQUEST_METHOD'];
 
