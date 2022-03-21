c4cc8703f324aab4cd25fdbb360553a6c0d83492
Benjamin Parks <Benparks101@gamil.comgit config --global user.email Benparks101@gamil.com>
2022-03-21 10:37:50 -0500

    Fix

--- api/authors/index.php
+++ api/authors/index.php
@@ -3,8 +3,8 @@
 header('Access-Control-Allow-Origin: *');
 header('Content-Type: application/json');
 
-include_once '../../checkingFunctions/fail.php';
-include_once '../../checkingFunctions/missingReqParams.php';
+include_once '../checkingFunctions/fail.php';
+include_once '../checkingFunctions/missingReqParams.php';
 
 
 $method = $_SERVER['REQUEST_METHOD'];
